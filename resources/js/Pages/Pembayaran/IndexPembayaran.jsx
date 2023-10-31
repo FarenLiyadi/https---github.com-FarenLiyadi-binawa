a70b3024fbe2113892272f37f7ce6cd77f412c65
Bryan-snw <Judithbryan_19@kharisma.ac.id>
2023-10-31 11:16:40 +0800

    fix indexPembayaran

--- resources/js/Pages/Pembayaran/IndexPembayaran.jsx
+++ resources/js/Pages/Pembayaran/IndexPembayaran.jsx
@@ -3,6 +3,7 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, Link, router } from "@inertiajs/react";
 
 export default function IndexPembayaran({ auth, pembayaran }) {
+    console.log(pembayaran);
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -15,70 +16,8 @@ export default function IndexPembayaran({ auth, pembayaran }) {
             <Head title="Histori Pembayaran" />
 
             <div className="py-12">
-                {auth.user.roles == "ADMIN"
+                {auth.user.roles == "USER"
                     ? pembayaran.data.map((data, index) => {
-                          return (
-                              <div key={index} className="py-2">
-                                  <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                                      <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                                          <div className="p-6 text-gray-900">
-                                              <p>{data.keterangan}</p>
-                                              <p>{data.jenis_pembayaran}</p>
-                                              <p>
-                                                  {new Date(
-                                                      data.tanggal_pembayaran
-                                                  ).toLocaleDateString(
-                                                      "id-ID",
-                                                      {
-                                                          weekday: "long",
-                                                          day: "2-digit",
-                                                          month: "long",
-                                                          year: "numeric",
-                                                      }
-                                                  )}
-                                              </p>
-                                              {data.approve ? (
-                                                  <span className="text-green-500 font-bold">
-                                                      Done
-                                                  </span>
-                                              ) : (
-                                                  <span className="text-red-500 font-bold">
-                                                      Not Checked Yet
-                                                  </span>
-                                              )}
-                                              {auth.user.roles != "USER" ? (
-                                                  <div>
-                                                      <p>
-                                                          Pembayaran Oleh:{" "}
-                                                          <span className="font-bold">
-                                                              {data.user.name}
-                                                          </span>
-                                                      </p>
-                                                      <div className="mt-2">
-                                                          <Link
-                                                              className="bg-blue-600 text-white font-bold px-5 py-2 rounded-lg"
-                                                              href={`/pembayaran/${data.id}`}
-                                                          >
-                                                              Lihat Detail
-                                                          </Link>
-                                                      </div>
-                                                  </div>
-                                              ) : (
-                                                  ""
-                                              )}
-                                          </div>
-                                      </div>
-                                      <div className="flex w-full justify-center pt-10">
-                                          <Pagination
-                                              class="mt-6"
-                                              links={pembayaran.meta}
-                                          />
-                                      </div>
-                                  </div>
-                              </div>
-                          );
-                      })
-                    : pembayaran.map((data, index) => {
                           return (
                               <div key={index} className="py-2">
                                   <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -137,6 +76,68 @@ export default function IndexPembayaran({ auth, pembayaran }) {
                                                   ""
                                               )}
                                           </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          );
+                      })
+                    : pembayaran.data.map((data, index) => {
+                          return (
+                              <div key={index} className="py-2">
+                                  <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                                      <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                                          <div className="p-6 text-gray-900">
+                                              <p>{data.keterangan}</p>
+                                              <p>{data.jenis_pembayaran}</p>
+                                              <p>
+                                                  {new Date(
+                                                      data.tanggal_pembayaran
+                                                  ).toLocaleDateString(
+                                                      "id-ID",
+                                                      {
+                                                          weekday: "long",
+                                                          day: "2-digit",
+                                                          month: "long",
+                                                          year: "numeric",
+                                                      }
+                                                  )}
+                                              </p>
+                                              {data.approve ? (
+                                                  <span className="text-green-500 font-bold">
+                                                      Done
+                                                  </span>
+                                              ) : (
+                                                  <span className="text-red-500 font-bold">
+                                                      Not Checked Yet
+                                                  </span>
+                                              )}
+                                              {auth.user.roles != "USER" ? (
+                                                  <div>
+                                                      <p>
+                                                          Pembayaran Oleh:{" "}
+                                                          <span className="font-bold">
+                                                              {data.user.name}
+                                                          </span>
+                                                      </p>
+                                                      <div className="mt-2">
+                                                          <Link
+                                                              className="bg-blue-600 text-white font-bold px-5 py-2 rounded-lg"
+                                                              href={`/pembayaran/${data.id}`}
+                                                          >
+                                                              Lihat Detail
+                                                          </Link>
+                                                      </div>
+                                                  </div>
+                                              ) : (
+                                                  ""
+                                              )}
+                                          </div>
+                                      </div>
+                                      <div className="flex w-full justify-center pt-10">
+                                          <Pagination
+                                              class="mt-6"
+                                              links={pembayaran.meta}
+                                          />
                                       </div>
                                   </div>
                               </div>
